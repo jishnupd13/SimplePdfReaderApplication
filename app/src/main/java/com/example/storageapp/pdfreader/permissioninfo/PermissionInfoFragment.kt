c7ce542c269ea423d06e386ba839dfcc8084eabf
Jishnu pdileep <jishnupd13@gmail.com>
2023-04-06 23:55:57 +0530

    permission completed

--- app/src/main/java/com/example/storageapp/pdfreader/permissioninfo/PermissionInfoFragment.kt
+++ app/src/main/java/com/example/storageapp/pdfreader/permissioninfo/PermissionInfoFragment.kt
@@ -1,21 +1,28 @@
 package com.example.storageapp.pdfreader.permissioninfo
 
-import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
 import android.content.Intent.*
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.view.View
+import android.view.ViewGroup
+import android.view.Window
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.storageapp.R
+import com.example.storageapp.databinding.AlertOpenSettingsPermissionBinding
 import com.example.storageapp.databinding.FragmentPermissionInfoBinding
 import com.example.storageapp.pdfreader.splash.SplashViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class PermissionInfoFragment : Fragment(R.layout.fragment_permission_info) {
@@ -24,11 +31,12 @@ class PermissionInfoFragment : Fragment(R.layout.fragment_permission_info) {
     private val viewModel by viewModels<SplashViewModel>()
     private lateinit var permissionsLauncher: ActivityResultLauncher<Array<String>>
 
-     private var settingsPermissionResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val data: Intent? = result.data
-        }
-    }
+     private var settingsPermissionResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+         lifecycleScope.launch(Dispatchers.Main) {
+             delay(600)
+             viewModel.checkPermissionNeeded(requireContext())
+         }
+     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -56,7 +64,7 @@ class PermissionInfoFragment : Fragment(R.layout.fragment_permission_info) {
                     val neverAskAgain = !ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), permission)
                     if (neverAskAgain) {
                         //user click "never ask again"
-                        openAppSettings()
+                        showOpenSettingsDialog()
                     } else {
                         //show explain dialog
                         viewModel.checkPermissionNeeded(requireContext())
@@ -67,8 +75,6 @@ class PermissionInfoFragment : Fragment(R.layout.fragment_permission_info) {
                 }
             }
         }
-
-
     }
 
     private fun openAppSettings(){
@@ -76,19 +82,32 @@ class PermissionInfoFragment : Fragment(R.layout.fragment_permission_info) {
         with(intent) {
             data = Uri.fromParts("package", requireContext().packageName, null)
             addCategory(CATEGORY_DEFAULT)
-            addFlags(FLAG_ACTIVITY_NEW_TASK)
-            addFlags(FLAG_ACTIVITY_NO_HISTORY)
-            addFlags(FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
-            addFlags(FLAG_ACTIVITY_CLEAR_TOP)
-            addFlags(FLAG_ACTIVITY_CLEAR_TASK)
         }
-        startActivity(intent)
+        settingsPermissionResultLauncher.launch(intent)
     }
 
 
     private fun observeReadAndWritePermissionLiveData(){
         viewModel.permissionLiveDate.observe(viewLifecycleOwner){
-            permissionsLauncher.launch(it.toTypedArray())
+            if(it.isEmpty()){
+                findNavController().navigate(PermissionInfoFragmentDirections.actionPermissionInfoFragmentToHomeFragment())
+            }else{
+                permissionsLauncher.launch(it.toTypedArray())
+            }
+        }
+    }
+
+    private fun showOpenSettingsDialog(){
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        val binding  = AlertOpenSettingsPermissionBinding .inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.show()
+        binding.btnOpenSettings.setOnClickListener{
+        dialog.dismiss()
+        openAppSettings()
         }
     }
 }
